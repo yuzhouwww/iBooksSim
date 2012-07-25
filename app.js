5e843058e0f18cdd75a4422cc57ed30914abf2cb
yuzhouwww <469258988@qq.com>
2012-07-25 09:42:04 +0800

    改为多个overflow

--- app.js
+++ app.js
@@ -9,13 +9,25 @@
 $(function () {
     var chapters = [5, 3, 7, 4, 8];
     var chapterPoint = [], currentPoint = 0;
-    for (var chapter = 0; chapter < chapters.length; chapter++) {
-        console.log(chapter+1+'页');
-        var chapterDiv = $('<div class="chapter"></div>').appendTo('#chapterScrollContent').html(chapter+1).css({
-            left:chapter * 1024
+    for (var chapter = 0,pageScroll; chapter < chapters.length; chapter++) {
+        console.log(chapter + 1 + '页');
+        var chapterDiv = $('<div class="chapter"></div>').html(chapter + 1).css({
+            left:(chapter) * 1024
         });
 
-        var pageScroll = $('<div class="pageScroll"></div>').appendTo(chapterDiv);
+        pageScroll = $('<div class="pageScroll"></div>').appendTo(chapterDiv);
+        pageScroll.on('touchstart touchmove touchend', function (e) {
+            console.log($(e.target).width());
+            if (e.type == 'touchstart') {
+                pageScrolling = true;
+            }
+            else if (e.type == 'touchmove') {
+
+            }
+            else if (e.type == 'touchend') {
+                pageScrolling = false;
+            }
+        });
         $('<div class="pageScrollBlank"></div>').appendTo(pageScroll);
 
         for (var page = 0; page < chapters[chapter]; page++) {
@@ -29,13 +41,30 @@ $(function () {
                     height:150
                 });
             }
-
         }
+        chapterDiv.appendTo('#chapterScrollContent');
     }
-    var currentIndex = 0, touchBegin, touchLast, lastDistance, totalDistance;
+
+//    setTimeout(function(){
+//        $('.pageScroll').on('touchstart touchmove touchend', function (e) {
+//            console.log($(e.target).width());
+//            if (e.type == 'touchstart') {
+//                pageScrolling = true;
+//            }
+//            else if (e.type == 'touchmove') {
+//
+//            }
+//            else if (e.type == 'touchend') {
+//                pageScrolling = false;
+//            }
+//        });
+//    },0);
+
+    var currentIndex = 1, touchBegin, touchLast, lastDistance, totalDistance;
     var pageScrolling;
     $('#chapterScroll').on('touchstart touchmove touchend', function (e) {
-        if(pageScrolling){
+//        console.log($(e.target).width());
+        if (pageScrolling) {
             return;
         }
         if (e.type == 'touchstart') {
@@ -74,26 +103,21 @@ $(function () {
                     currentIndex += 1;
                 }
             }
-            $('#chapterScrollContent').css({
-                '-webkit-transform':'translate3d(' + (-currentIndex) * 1024 + 'px,0,0)',
-                '-webkit-transition-duration':'0.5s'
-            });
+            goToPage(currentIndex);
         }
 //        return false;
     });
 
-    console.log('length'+$('.pageScroll').length);
-    $('.pageScroll').on('touchstart touchmove touchend', function (e) {
-        console.log(this.scrollLeft);
-        if(e.type == 'touchstart'){
-            pageScrolling = true;
-        }
-        else if(e.type == 'touchmove'){
+    function goToPage(index) {
+        $('#chapterScrollContent').css({
+            '-webkit-transform':'translate3d(' + (-index) * 1024 + 'px,0,0)',
+            '-webkit-transition-duration':'0.5s'
+        });
+    }
 
-        }
-        else if(e.type == 'touchend'){
-            pageScrolling = false;
-        }
-    });
+    goToPage(currentIndex);
+
+    console.log('length' + $('.pageScroll').length);
+
 
 });
